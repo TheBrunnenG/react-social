1395fcd4c65293d6693f48ca83c3c51b7c4bff8a
Oleg <krallakane@gmail.com>
2020-05-27 12:20:01 +0500

    added build project

---
+++ precache-manifest.b80f3cbb4202beb92789cae1338d309a.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "596c7fe0401589f18c815978af0e78c4",
+    "url": "/react-social/index.html"
+  },
+  {
+    "revision": "b5831406b4803a846979",
+    "url": "/react-social/static/css/3.f8a90726.chunk.css"
+  },
+  {
+    "revision": "5602f742fd6924784bf5",
+    "url": "/react-social/static/css/4.bd7d464b.chunk.css"
+  },
+  {
+    "revision": "0353e05f1b682313b327",
+    "url": "/react-social/static/css/main.ba1ecdf0.chunk.css"
+  },
+  {
+    "revision": "65b66ba3f66353bc23e9",
+    "url": "/react-social/static/js/2.543b37b4.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/react-social/static/js/2.543b37b4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b5831406b4803a846979",
+    "url": "/react-social/static/js/3.67d4f6cd.chunk.js"
+  },
+  {
+    "revision": "5602f742fd6924784bf5",
+    "url": "/react-social/static/js/4.e02e6745.chunk.js"
+  },
+  {
+    "revision": "0353e05f1b682313b327",
+    "url": "/react-social/static/js/main.7cc5ab90.chunk.js"
+  },
+  {
+    "revision": "3046e80c42cf7a004c29",
+    "url": "/react-social/static/js/runtime-main.b15c5e93.js"
+  },
+  {
+    "revision": "27fc27b682d7147fc0935299c8467922",
+    "url": "/react-social/static/media/loader.27fc27b6.svg"
+  }
+]);
